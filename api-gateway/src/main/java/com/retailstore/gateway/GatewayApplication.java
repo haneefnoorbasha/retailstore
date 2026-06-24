88bfcd4caaa75083f28eac9cc8e91d69fa15ccd6
Haneef NB <haneef.noorbasha@gmail.com>
2026-06-23 23:09:18 -0500

    Test 1

--- api-gateway/src/main/java/com/retailstore/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/retailstore/gateway/GatewayApplication.java
@@ -4,6 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) { SpringApplication.run(GatewayApplication.class, args);
-        System.out.println("---API Gateway started---");
+        System.out.println("---API Gateway started 1---");
     }
 }
